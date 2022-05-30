2590c0b7320f7a6fe3cb85064451aca35eed7368
David Silva <Davidslv@users.noreply.github.com>
2022-05-30 18:52:39 +0100

    Add Checkout#scan
    This method returns true when an item is found in ItemStorage.
    This method returns false when an item is not found in ItemStorage.
    
    This enables some kind of feedback when the scan method is used.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
+require 'item_storage'
+
 class Checkout
   def initialize(rules = [])
     @rules = rules
+    @basket = []
+    @items = ItemStorage.new
+  end
+
+  def scan(item_code)
+    item = @items.find(item_code)
+    return false unless item
+
+    @basket << item
+    true
   end
 end
--- spec/lib/checkout_spec.rb
+++ spec/lib/checkout_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe Checkout do
   describe '#new' do
     it { expect{ described_class.new }.to_not raise_error(ArgumentError) }
   end
+
+  describe '#scan' do
+    subject(:checkout) { described_class.new }
+
+    context 'when item exists' do
+      it 'returns true' do
+        expect(checkout.scan(001)).to be(true)
+      end
+    end
+
+    context 'when item does not exist' do
+      it 'returns false' do
+        expect(checkout.scan(000)).to be(false)
+      end
+    end
+  end
 end
