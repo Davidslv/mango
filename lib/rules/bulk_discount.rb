b3188dbd9253d1f61686cf2426bd886255453f5d
David Silva <Davidslv@users.noreply.github.com>
2022-06-06 19:03:26 +0100

    Refactor Rules::BulkDiscount

--- lib/rules/bulk_discount.rb
+++ lib/rules/bulk_discount.rb
@@ -17,15 +17,15 @@ module Rules
     private
 
     def eligible?(basket)
-      !!eligible_item(basket) && count_eligible_item(basket) >= 2
+      eligible_item(basket) && count_eligible_item(basket) >= @minimum_quantity
     end
 
     def eligible_item(basket)
-      basket.find { |item| item.code == @item.code }
+      basket.include?(@item)
     end
 
     def count_eligible_item(basket)
-      basket.count { |item| item.code == @item.code }
+      basket.count(@item)
     end
   end
 end
