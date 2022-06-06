5e07950f9b56d4d5590b7d1a05b3a80455ca5723
David Silva <Davidslv@users.noreply.github.com>
2022-06-06 16:18:29 +0100

    Update Rules::BulkDiscount to be flexible
    This improves the design of Rules::BulkDiscount since it no longer needs
    the hardcode constants about a given item.
    
    Now it can receive any item and the conditions for that specific item.

--- spec/lib/rules/bulk_discount_spec.rb
+++ spec/lib/rules/bulk_discount_spec.rb
@@ -1,49 +1,42 @@
-require 'item'
+require 'item_storage'
 require './lib/rules/bulk_discount'
 
 module Rules
   RSpec.describe BulkDiscount do
-    subject { described_class.new(basket, nil) }
-    let(:basket) { [] }
+    let(:item) { ItemStorage.new.find(001) }
 
-    describe '#eligible?' do
-      context 'when basket is empty' do
-        it 'returns false' do
-          expect(subject.eligible?).to be(false)
-        end
-      end
-
-      context 'when basket has two eligble items' do
-        let(:basket) {
-          [
-            Item.new(code: 001, name: 'lavender', price: 9.25),
-            Item.new(code: 001, name: 'lavender', price: 9.25)
-          ]
-        }
-
-        it 'returns true' do
-          expect(subject.eligible?).to be(true)
-        end
-      end
+    let(:conditions) do
+      {
+        item: item,
+        minimum_quantity: 2,
+        price_reduction: 0.75
+      }
     end
 
+    subject { described_class.new(conditions) }
+
     describe '#discount' do
-      let(:basket) {
-        [
-          Item.new(code: 001, name: 'lavender', price: 9.25),
-          Item.new(code: 001, name: 'lavender', price: 9.25)
-        ]
-      }
+      let(:basket) { [item, item] }
 
       it 'returns the value of the discount to be reduced from the total' do
-        expect(subject.discount).to eq(1.50)
+        expect(
+          subject.discount(basket: basket)
+        ).to eq(1.50)
       end
 
       context 'when only one eligible item' do
-        let(:basket) { [ Item.new(code: 001, name: 'lavender', price: 9.25) ] }
+        let(:basket) { [item] }
 
         it 'returns zero' do
-          expect(subject.discount).to eq(0)
+          expect(subject.discount(basket: basket)).to eq(0)
+        end
+      end
+
+      context 'when basket is empty' do
+        let(:basket) { [] }
+
+        it 'returns zero' do
+          expect(subject.discount(basket: basket)).to eq(0)
         end
       end
     end
